91e9324f794de62953bd4ee37efef2951dd5525b
Jacob Sides <sides798@gmail.com>
2022-07-15 18:00:30 +0000

    SvelteKit my-template init and base line rotate

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import heroImg from '$static/hero.jpg';
+
+</script>
+
+<!-- A <main> is the same as <div>, but more clear -->
+<main
+	class="px-4 xl:px-0 py-8 relative min-h-screen bg-[#141516]"
+>	
+	<i class=""></i>
+</main>
