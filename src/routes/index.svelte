dafaa190ee034585f4a4b8a010c2e32b053a3367
Jacob Sides <sides798@gmail.com>
2022-07-15 19:10:51 +0000

    Grid and animation interaction

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,27 @@
 <script>
-	import heroImg from '$static/hero.jpg';
+	import { onMount } from 'svelte';
 
+	let cells = [];
+	const cellSize = 16;
+	function buildCells() {
+		const perRow = window.innerWidth / cellSize;
+		const perCol = window.innerHeight / cellSize;
+		cells = Array(Math.ceil(perRow * perCol + 5 + perRow));
+	}
+	onMount(() => {
+		buildCells();
+		window.addEventListener('resize', buildCells);
+	});
+	function restartElm(e) {
+		e.target.getAnimations()[0].currentTime = 0;
+	}
 </script>
 
 <!-- A <main> is the same as <div>, but more clear -->
-<main
-	class="px-4 xl:px-0 py-8 relative min-h-screen bg-[#141516]"
->	
-	<i class=""></i>
+<main class=" relative min-h-screen bg-[#141516] overflow-hidden h-screen w-screen">
+	<section class="flex flex-wrap content-start w-[102vw] h-[102vh]">
+		{#each cells as cell}
+			<i data-value={cell} on:mouseover={restartElm} on:focus />
+		{/each}
+	</section>
 </main>
